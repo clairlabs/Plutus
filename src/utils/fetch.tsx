146668f9d93f104d8b778b55958f8ff80047fdd9
SSA Lionheart <ssalionheart@gmail.com>
2023-10-13 17:29:16 +0000

    Delete fetch.tsx

--- src/utils/fetch.tsx
+++
@@ -1,9 +0,0 @@
-const fetch = async (url: string, options = {method: "GET"}): Promise<Record<string, any>> => {
-    try {
-        const response = await fetch(url, options);
-
-        return await response.json();
-    }
-}
-
-export default fetch
